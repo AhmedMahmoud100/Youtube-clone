2a73b463477edb07fe64219dcb09b5e97e1c12ec
Ahmed Mahmoud <ahmedopo100@gmail.com>
2023-01-09 16:06:00 +0200

    last commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns : {
+        'autoFit' : 'repeat(auto-fit, minmax(230px, 1fr))'
+      }
+    },
   },
   plugins: [require("@tailwindcss/line-clamp")],
 }
